b024603119b3d8e459b8e0f1fa99a7c14a61c4de
Staryo40 <arrrryow@gmail.com>
2025-08-18 00:07:15 +0700

    feat: Stringbit basic operations

---
+++ src/main/java/irk/staryo/utils/StringBitOperation.java
@@ -0,0 +1,45 @@
+package irk.staryo.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringBitOperation {
+    public static int stringBitOrder(String bits){
+        int result = 0;
+        for (Character c : bits.toCharArray()){
+            if (c == '1'){
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+    public static String findCommonBits(String s1, String s2){
+        assert s1.length() == s2.length() : "Both bit string needs to be the same length";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s1.length(); i++){
+            if (s1.charAt(i) == '1' && s2.charAt(i) == '1'){
+                sb.append('1');
+            } else {
+                sb.append('0');
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static List<String> getLowerOrderSubsets(String s){
+        List<String> lowerOrderSubsets = new ArrayList<>();
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '1') {
+                char[] tempChars = s.toCharArray();
+                tempChars[i] = '0';
+                lowerOrderSubsets.add(new String(tempChars));
+            }
+        }
+        return lowerOrderSubsets;
+    }
+}
